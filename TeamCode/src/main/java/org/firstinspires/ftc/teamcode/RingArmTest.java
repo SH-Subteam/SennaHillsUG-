96b1414caa4b67f6d8c3bc40a4f9c14525c10b2e
Akash Vasan <akash.vasan11@gmail.com>
2020-11-09 18:32:34 -0600

    Deletes the SHHardware class and adds the new servo ports to the DMHardware class, also changed the object "robot" of the RingArmTest class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingArmTest.java
@@ -7,24 +7,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 @Autonomous(name = "ArmRingTest", group = "DreamMachines")
 public class RingArmTest extends LinearOpMode{
 
-    public SHHardware robot = new SHHardware(false);
+    public DMHardware robot = new DMHardware(false);
 
     @Override
     public void runOpMode() {
         robot.initTeleOpIMU(hardwareMap);
         robot.timer.reset();
 
-        robot.arm.setPosition(1);
+        robot.ringArm.setPosition(1);
 
         waitForStart();
 
-        robot.arm.setPosition(0);
+        robot.ringArm.setPosition(0);
 
         while (robot.getTime() <= 5) {
 
         }
 
-        robot.arm.setPosition(1);
+        robot.ringArm.setPosition(1);
 
         robot.timer.reset();
     }
